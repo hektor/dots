fbad53561b87eb9e5ba537b69fa1365299cf73f6
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-13 17:09:15 +0000

    Adjust dwm colors

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -14,7 +14,7 @@ static const char col_gray3[]       = "#444444";
 static const char col_gray4[]       = "#eeeeee";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray2, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_gray1,  col_gray3  },
 };
 
