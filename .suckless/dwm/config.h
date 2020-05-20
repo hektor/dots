85cf630772b230501b30338e35957ac0a5d18df8
Hektor Misplon <hektor.misplon@gmail.com>
2020-05-20 19:13:08 +0000

    Adjust dmenu colors

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray4, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray2, "-nf", col_gray3, "-sb", col_gray4, "-sf", col_gray2, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
