54e0790a8aa37646243b5f719b19c6065d9f655d
Hektor Misplon <hektor.misplon@gmail.com>
2021-02-08 17:51:56 +0100

    Run dmenu-run-history from dwm

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray1, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu-run-history", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray1, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *prtscrcmd[] = { "flameshot", "gui", NULL};
 static const char *incvol[] = { "amixer", "set", "Master", "3+", NULL };
