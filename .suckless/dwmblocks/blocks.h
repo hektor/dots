b07e7a7daa26d5774e39dea590f5daf7a2102dc9
Hektor Misplon <hektor.misplon@pm.me>
2021-11-28 17:47:17 +0100

    Update dwmblocks

--- .suckless/dwmblocks/blocks.h
+++ .suckless/dwmblocks/blocks.h
@@ -2,9 +2,11 @@
 static const Block blocks[] = {
   /*Icon*/  /*Command*/   /*Update Interval*/ /*Update Signal*/
 /*  {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30,   0}, */
-  {"", "current-song",                     20,   0},
-  {"", "current-bt-audio",                 20,   0},
-  {"", "date '+%a %d %b %I:%M%p'",          5,    0},
+  {"", "cat /tmp/pomo",             1,  0},
+  {"Week ", "date '+%V'",           60, 0},
+  {"", "check-battery-level",       60, 0},
+  {"", "date '+%a %d %b %I:%M%p'",  5,  0},
+  {"", "task status:pending count", 20, 0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
