6abdd0e8a7aeb05c37770d6a2ca7263ec24901f3
Hektor Misplon <hektor.misplon@pm.me>
2022-05-12 09:08:10 +0200

    Rearrange

--- .suckless/dwmblocks/blocks.h
+++ .suckless/dwmblocks/blocks.h
@@ -7,13 +7,13 @@ static const Block blocks[] = {
    * Update interval
    * Update signal
    */
-  {"", "sb-date",      60, 0}, // Week, time, day
-  {"", "sb-pomo",      1,  0}, // Pomodoro timer
-  {"", "sb-tasks",     5,  0}, // Tasks done
-  {"", "sb-anki",   5,  0},    // Reviews done
-  {"", "sb-internet",  5,  0}, // Ethernet & WiFi status
-  {"", "sb-battery",   60, 0}, // Battery level
-  {"", "sb-portfolio", 600, 0} // Stocks
+  {"", "sb-date",      60,  0}, // Week, time, day
+  {"", "sb-pomo",      1,   0}, // Pomodoro timer
+  {"", "sb-tasks",     5,   0}, // Tasks done
+  {"", "sb-anki",      5,   0}, // Reviews done
+  {"", "sb-internet",  5,   0}, // Ethernet & WiFi status
+  {"", "sb-portfolio", 600, 0}, // Stocks
+  {"", "sb-battery",   60,  0}, // Battery level
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
