2c1e4303abda6d788ec6c901a109a37839badd1b
Hektor Misplon <hektor.misplon@pm.me>
2022-08-27 10:27:27 +0200

    Add firefox config

---
+++ .config/firefox/user.js
@@ -0,0 +1,10 @@
+/*
+ * Note: has to be symlinked to profile directories for your
+ *       firefox release
+ *
+ * ```sh
+ * ln user.js ~/.mozilla/firefox/*.default-release/user.js
+ * ```
+ */
+
+user_pref('browser.download.dir', '/home/h/dl')
