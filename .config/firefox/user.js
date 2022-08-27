4407e35765b9ad5a14cc689658d404b1858b1efd
Hektor Misplon <hektor.misplon@pm.me>
2022-08-27 10:41:45 +0200

    Add firefox config setup script

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -2,9 +2,13 @@
  * Note: has to be symlinked to profile directories for your
  *       firefox release
  *
+ * E.g.
+ *
  * ```sh
- * ln user.js ~/.mozilla/firefox/*.default-release/user.js
+ * ln -s user.js ~/.mozilla/firefox/*.default-release/user.js
  * ```
+ * Or check out the `setup` script
+ *
  */
 
 user_pref('browser.download.dir', '/home/h/dl')
