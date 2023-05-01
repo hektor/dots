5fd5686fd47d9a70ada2810fc17c1b775917b1a7
Hektor Misplon <hektor.misplon@pm.me>
2023-05-01 15:27:25 +0200

    Untrack wiki files

--- .wiki/filters/html-links.lua
+++
@@ -1,9 +0,0 @@
-function Link(el)
-  --Replace markdown extension with html extension
-  if string.match(el.target, '.md$') then
-    el.target = string.gsub(el.target, "%.md", "")
-    --Add html extension
-    el.target = el.target .. '.html'
-  end
-  return el
-end
