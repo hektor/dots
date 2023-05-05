be4ddc5a89522288087f10759884d9689a4909c6
hektor <hektor.misplon@pm.me>
2023-05-05 20:31:42 +0200

    Update taskwarrior config

--- .config/task/taskrc.d/setup.sh
+++ .config/task/taskrc.d/setup.sh
@@ -6,9 +6,9 @@ source ~/.env
 
 config_file=~/.config/task/taskrc.d/server
 
-echo "taskd.certificate=~/.task/default-client.cert.pem" > $config_file
-echo "taskd.key=~/.task/default-client.key.pem" >> $config_file
-echo "taskd.ca=~/.task/ca.cert.pem" >> $config_file
+echo "taskd.certificate=$XDG_DATA_HOME/task/default-client.cert.pem" > $config_file
+echo "taskd.key=$XDG_DATA_HOME/task/default-client.key.pem" >> $config_file
+echo "taskd.ca=$XDG_DATA_HOME/task/ca.cert.pem" >> $config_file
 echo "taskd.trust=ignore hostname" >> $config_file
 echo "taskd.server=$TASKD_SERVER:$TASKD_PORT" >> $config_file
 echo "taskd.credentials=$TASKD_CREDS" >> $config_file
